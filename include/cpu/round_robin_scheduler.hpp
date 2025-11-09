52448391e173428ca0425df819f0610eef3b48b5
gustadev24 <lgsc21211@gmail.com>
2025-11-09 17:54:27 -0500

    feat: implement round robin scheduler method

---
+++ include/cpu/round_robin_scheduler.hpp
@@ -0,0 +1,31 @@
+#ifndef ROUND_ROBIN_SCHEDULER_HPP
+#define ROUND_ROBIN_SCHEDULER_HPP
+
+#include "cpu/scheduler.hpp"
+#include <deque>
+
+namespace OSSimulator {
+
+class RoundRobinScheduler : public Scheduler {
+private:
+  std::deque<Process> ready_queue;
+  int quantum;
+
+public:
+  explicit RoundRobinScheduler(int q = 4);
+
+  void add_process(const Process &process) override;
+  Process *get_next_process() override;
+  bool has_processes() const override;
+  void remove_process(int pid) override;
+  void rotate();
+  size_t size() const override;
+  void clear() override;
+  int get_quantum() const;
+  void set_quantum(int q);
+  SchedulingAlgorithm get_algorithm() const override;
+};
+
+} // namespace OSSimulator
+
+#endif // ROUND_ROBIN_SCHEDULER_HPP
